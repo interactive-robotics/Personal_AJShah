eb2d5d681dd5319812139a39dbd2c457f843c3ea
Ankit Shah <ajshah@mit.edu>
2017-08-16 14:11:18 -0400

    added LM classification code

---
+++ LMProject/RNNDataParser.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 11 19:42:23 2017
+
+@author: ajshah
+"""
+
+import numpy as np
+import pandas as pd
+from PrepareFeaturesRNN import *
+
+scenarios = ['1A','1B','1C', '2A','2B','2C', '3A','3B','3C', '4A','4C']
+TestScenario = ['4C']
+
+def ReadRNNData(scenarios, TestScenario, FeatureClass, WindowSize=5, SeqSize=100):
+    
+    
+    if FeatureClass == 'OwnshipData':
+        GetData = GetTestAndTrainDataOwnship
+    elif FeatureClass == 'OwnshipWingmanData':
+        GetData = GetTestAndTrainDataOwnshipWingman
+    
+    [X_train, y_train, X_test, y_test, TrainStartID, TrainEndID, TestStartID, TestEndID] = GetData(scenarios, TestScenario, WindowSize)
+    
+    LabelDict = np.array(['Push','Legs','OnTarget','Egress','ThreatIdentification','ThreatAvoidanceMitigation'],dtype=object)
+    y_test = np.array(pd.get_dummies(pd.Series(pd.Categorical(y_test,categories = LabelDict))))
+    y_train = np.array(pd.get_dummies(pd.Series(pd.Categorical(y_train,categories=LabelDict))))
+    
+    #SeqSize = 500
+    
+    SeqX_train = np.zeros((0,SeqSize, X_train.shape[1]))
+    SeqY_train = np.zeros((0,SeqSize, y_train.shape[1]))
+    
+    for i in range(len(TrainStartID)):
+    #for i in [1]:
+        ScenarioData = X_train[TrainStartID[i]:TrainEndID[i],:]
+        ScenarioLabels = y_train[TrainStartID[i]:TrainEndID[i],:]
+        dataChops = np.arange(0, ScenarioData.shape[0], SeqSize)
+        for j in range(len(dataChops)-1):
+            newSeqData = ScenarioData[dataChops[j]:dataChops[j+1],:]
+            newSeqData = np.reshape(newSeqData,(1,*newSeqData.shape))
+            SeqX_train = np.append(SeqX_train, newSeqData, axis=0)
+            
+            newLabels = y_train[dataChops[j]:dataChops[j+1],:]
+            newLabels = np.reshape(newLabels,(1,*newLabels.shape))
+            SeqY_train = np.append(SeqY_train, newLabels,axis=0)
+        
+        LastData = ScenarioData[dataChops[-1]:,:]
+        LastSeqData = np.zeros((1,SeqSize,X_train.shape[1]))
+        LastSeqData[0,0:LastData.shape[0],:] = LastData
+        SeqX_train = np.append(SeqX_train,LastSeqData, axis=0)
+        
+        LastLabels = ScenarioLabels[dataChops[-1],:]
+        LastSeqLabels = np.zeros((1,SeqSize,y_train.shape[1]))
+        LastSeqLabels[0,0:LastLabels.shape[0],:] = LastLabels
+        SeqY_train = np.append(SeqY_train,LastSeqLabels,axis=0)
+    
+    SeqX_test = np.zeros((0,SeqSize,X_test.shape[1]))
+    SeqY_test = np.zeros((0,SeqSize,y_test.shape[1]))
+    
+    for i in range(len(TestStartID)):
+    #for i in [1]:
+        ScenarioData = X_test[TestStartID[i]:TestEndID[i],:]
+        ScenarioLabels = y_test[TestStartID[i]:TestEndID[i],:]
+        dataChops = np.arange(0, ScenarioData.shape[0], SeqSize)
+        for j in range(len(dataChops)-1):
+            newSeqData = ScenarioData[dataChops[j]:dataChops[j+1],:]
+            newSeqData = np.reshape(newSeqData,(1,*newSeqData.shape))
+            SeqX_test = np.append(SeqX_test, newSeqData, axis=0)
+            
+            newLabels = y_test[dataChops[j]:dataChops[j+1],:]
+            newLabels = np.reshape(newLabels,(1,*newLabels.shape))
+            SeqY_test = np.append(SeqY_test, newLabels,axis=0)
+        
+        LastData = ScenarioData[dataChops[-1]:,:]
+        LastSeqData = np.zeros((1,SeqSize,X_train.shape[1]))
+        LastSeqData[0,0:LastData.shape[0],:] = LastData
+        SeqX_test = np.append(SeqX_test,LastSeqData, axis=0)
+        
+        LastLabels = ScenarioLabels[dataChops[-1],:]
+        LastSeqLabels = np.zeros((1,SeqSize,y_train.shape[1]))
+        LastSeqLabels[0,0:LastLabels.shape[0],:] = LastLabels
+        SeqY_test = np.append(SeqY_test,LastSeqLabels,axis=0)
+    
+    return SeqX_train, SeqY_train, SeqX_test, SeqY_test
+
+if __name__ == '__main__':
+    scenarios = ['1A','1B','1C', '2A','2B','2C', '3A','3B','3C', '4A','4C']
+    TestScenario = ['4C']
+    SeqX_train, SeqY_train, SeqX_test, SeqY_test = ReadRNNData(scenarios, TestScenario, 'OwnshipData')
+    
---
+++ LMProject/TrainModel_NN.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 16:54:51 2017
+
+@author: ajshah
+"""
+
+import pandas as pd
+import numpy as np
+from PrepareFeatures import *
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.activations import softmax
+from keras.utils import plot_model
+from keras.optimizers import adam
+from keras import callbacks
+import pickle
+from itertools import product
+
+
+def TrainRNN(scenarios, TestScenario, FeatureClass, WindowSize=5, BatchSize=100):
+#Prepare the data
+
+    TrainScenarios = list(set(scenarios) - set(TestScenario))
+    
+    if FeatureClass == 'OwnshipData':
+        GetData = GetTestAndTrainDataOwnship
+    elif FeatureClass == 'OwnshipWingmanData':
+        GetData = GetTestAndTrainDataOwnshipWingman
+
+    [X_train, y_train, X_test, y_test] = GetData(TrainScenarios, TestScenario, WindowSize = WindowSize)
+    y_train= pd.Series(y_train)
+    y_train = pd.Categorical(y_train, categories = list(set(y_train)))
+    y_train = pd.Series(y_train)
+    y_test = pd.Categorical(y_test, categories = list(set(y_train)))
+    y_test = pd.Series(y_test)
+    TrainSize = y_train.shape[0]
+    y_big = y_train.append(y_test)
+    y_big = pd.get_dummies(y_big)
+    y_train_onehot = np.array(y_big.iloc[0:TrainSize,:])
+    y_test_onehot  = np.array(y_big.iloc[TrainSize:,:])
+    
+    
+    
+    #Define a deep neural network
+    
+    NInputFeatures = X_train.shape[1]
+    Ntargets = len(set(y_train))
+    
+    NUnits = [NInputFeatures, NInputFeatures,100, 50,25]
+    
+    model = Sequential()
+    
+    model.add(Dense(units=NUnits[0], input_shape=(NInputFeatures,)))
+    
+    model.add(Dense(units = NUnits[1]))
+    
+    for i in range(2,len(NUnits)):
+        model.add(Dense(units=NUnits[i], activation='relu'))
+    
+    model.add(Dense(Ntargets))
+    model.add(Activation('softmax'))
+    
+    model.compile(loss = 'categorical_crossentropy', optimizer = adam(lr=0.001), metrics = ['accuracy'])
+    model.fit(X_train, y_train_onehot, batch_size = BatchSize, epochs = 20, validation_split = 0.05)
+    
+#    model.compile(loss = 'categorical_crossentropy', optimizer = adam(lr=0.0005), metrics = ['accuracy'])
+#    model.fit(X_train, y_train_onehot, batch_size = 50, epochs = 20, validation_split = 0.05)
+    metrics = model.evaluate(X_test, y_test_onehot)
+    Test_acc = metrics[1]
+    y_test_pred = model.predict(X_test)
+    labelDict = np.array(y_big.columns)
+    y_test_pred_int = np.argmax(y_test_pred, axis=1)
+    y_pred_labels = [labelDict[val] for val in y_test_pred_int]
+    
+    return model, Test_acc, y_pred_labels, labelDict
+
+def LOOCV(Scenarios, TestScenarios, FeatureClass, WindowSize=5, BatchSize=100):
+    models = {}
+    Test_accs = []
+    y_pred_labels = {}
+
+    for TestScenario in TestScenarios:
+        
+        new_model,new_Test_acc,new_y_pred_labels,LabelDict = TrainRNN(Scenarios, [TestScenario], FeatureClass, WindowSize, BatchSize)
+        
+        filename = 'NNModels/Model_'+str(WindowSize)+'_'+str(BatchSize)+'_'+TestScenario+'.h5'
+        DatFilename = 'NNModels/Model_'+str(WindowSize)+'_'+str(BatchSize)+'_'+TestScenario+'.pkl'
+        
+        new_model.save(filename)
+        
+        with open(DatFilename,'wb') as file:
+            pickle.dump({'Test_accuracy':new_Test_acc, 'Test_predictions':new_y_pred_labels, 'LabelDictionary':LabelDict},file)
+            
+        models[TestScenario] = filename
+        Test_accs.append(new_Test_acc)
+        y_pred_labels[TestScenario] = new_y_pred_labels
+        
+    return models, Test_accs, y_pred_labels
+
+def GridSearch(Scenarios, TestScenarios, FeatureClass, WindowSizes=[2,5,10], BatchSizes=[50,500]):
+    TestAccs = {}
+    MeanTestAccs = {}
+    for (WindowSize,BatchSize) in product(WindowSizes,BatchSizes):
+        
+        newModels, newTestAccs, NewPredLabels = LOOCV(Scenarios, TestScenarios, FeatureClass, WindowSize, BatchSize)
+        
+        TestAccs[(WindowSize,BatchSize)] = newTestAccs
+        newMeanTestAccs = np.mean(np.array(newTestAccs))
+        MeanTestAccs[(WindowSize,BatchSize)] = newMeanTestAccs
+    return TestAccs, MeanTestAccs
+
+if __name__ == '__main__':
+    
+#     #Test single scenario run
+#    Scenarios = ['1A','1B','1C','2A','2B','2C','3A','3B', '3C','4A','4C']
+#    TestScenario = ['1A']
+#    model, test_acc, y_pred_labels, LabelDict = TrainRNN(Scenarios, TestScenario, 'OwnshipData')
+
+    # Test LOOCV
+    Scenarios = ['1A','1B','1C','2A','2B','2C','3A','3B', '3C','4A','4C']
+    TestScenarios = ['1A','1B','1C','2A','2B','2C','3A','3B', '3C','4A','4C']
+    FeatureClass = 'OwnshipWingmanData'
+    models, Test_accs, y_pred_labels = LOOCV(Scenarios, TestScenarios, FeatureClass=FeatureClass)
+    with open('NNResults_'+FeatureClass+'.pkl','wb') as file:
+        pickle.dump({'Models':models, 'TestAccuracies':Test_accs, 'PredictedLabels':y_pred_labels},file)
+
+##     #Test GridSearch
+#    Scenarios = ['1A','1B','1C','2A','2B','2C','3A','3B', '3C','4A','4C']
+#    TestScenarios = ['1A','1B','1C','2A','2B','2C','3A','3B', '3C','4A','4C']
+#    TestAccs, MeanTestAccs = GridSearch(Scenarios, TestScenarios, 'OwnshipData')
