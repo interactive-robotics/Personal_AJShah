5c5353012788873700dcf80cfc73c6cfc2bedfeb
Shen Li <shentheman.c@gmail.com>
2020-09-08 15:55:08 -0400

    adjusting trial numbers

--- puns-bsi-client/Task_A_Remote.py
+++ puns-bsi-client/Task_A_Remote.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
 	
 	subject_id = sys.argv[1]
 	#active_trial_remote(nQuery=3, n_postdemo = 3, n_demo = 2)
-	active_trial_remote(nQuery=1, n_postdemo = 1, n_demo = 2, trials = 1)
+	active_trial_remote(nQuery=3, n_postdemo = 3, n_demo = 2, trials = 1)
 	record_subject_data(subject_id,'Active')
--- puns-bsi-client/Task_B_Remote.py
+++ puns-bsi-client/Task_B_Remote.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == '__main__':
 	
 	subject_id = sys.argv[1]
-	random_trial_remote(nQuery=1, n_postdemo = 1, n_demo = 2)
+	random_trial_remote(nQuery=3, n_postdemo = 3, n_demo = 2, trials=1)
 	record_subject_data(subject_id,'Random')
