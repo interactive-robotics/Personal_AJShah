a4f61fc0f0445ce9050c3ff2570953b2d042294d
ajshah91 <ajshah@mit.edu>
2021-02-08 15:27:50 -0500

    trial parallelism implemented

---
+++ MetaQuerying_Parallel2/params/meta_params.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+data_path = 'Meta_Data'
+raw_data_path = data_path+'/RawData'
+compressed_data_path = data_path + '/CompressedData'
+distributions_path = data_path + '/Distributions'
+nSamples = 20000
+nBurn = 100
+nTraj = 2
---
+++ MetaQuerying_Parallel2/sns_defaults.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 13:50:02 2020
+
+@author: AJShah
+"""
+
+rc = {'axes.labelsize': 28, 'axes.titlesize': 32, 'legend.fontsize': 24, 'xtick.labelsize': 24, 'ytick.labelsize': 22}
