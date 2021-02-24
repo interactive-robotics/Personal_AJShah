0308b98823d77a64975de548b91cf58eb6b60151
ajshah91 <ajshah@mit.edu>
2021-02-24 13:40:13 -0500

    working code for rejecting incorrect demonstrations

--- MetaQuerying_Pedagogical/pedagogical_demo.py
+++ MetaQuerying_Pedagogical/pedagogical_demo.py
@@ -8,18 +8,18 @@ from tqdm import tqdm
 
 def compute_updated_cross_entropy(formula, new_dist):
 
-    similarities = [compare_formulas(formula, form) for form in new_dist]
+    similarities = [compare_formulas(formula, form) for form in new_dist['formulas']]
     most_similar_idx = np.argmax(similarities)
 
-    return -log(new_dist['formulas'][most_similar_idx])
+    return -np.log(new_dist['probs'][most_similar_idx])
 
-def identify_pedagogical_state(groud_truth, prior_specification_fsm):
+def identify_pedagogical_state(ground_truth, prior_specification_fsm, debug = False):
 
     cross_entropies = []
     states = list(prior_specification_fsm.states2id.keys())
 
     for state in states:
-        new_dist = compute_online_bsi_update(state, specification_fsm: SpecificationFSM, label, n_threats = 5, n_waypoints = 5)
+        new_dist = compute_online_bsi_update(state, prior_specification_fsm, True, n_threats = 5, n_waypoints = 5)
         cross_entropies.append(compute_updated_cross_entropy(ground_truth, new_dist))
 
     state_idx = np.argmin(cross_entropies)
@@ -28,20 +28,23 @@ def identify_pedagogical_state(groud_truth, prior_specification_fsm):
     path_to_desired_state = nx.all_simple_paths(prior_specification_fsm.graph, 0, prior_specification_fsm.states2id[desired_state])
     bread_crumb_states = set([l for sublists in path_to_desired_state for l in sublists]) - set([prior_specification_fsm.states2id[desired_state]])
 
-    return desired_state, bread_crumb_states
+    if debug:
+        return {'desired_state': desired_state, 'breadcrumbs': bread_crumb_states, 'states': states, 'cross_entropies': cross_entropies}
+    else:
+        return desired_state, bread_crumb_states
 
 def recompile_reward_function_2(spec_fsm:SpecificationFSM, desired_states, breadcrumbs):
     a=1
 
 
-def create_pedagogical_demo(ground_truth, MDP, n_threats = 0):
+def create_pedagogical_demo(ground_truth, MDP, n_threats = 0, non_terminal = True, verbose = True):
 
     #Identify desired state in the current FSM
-    desired_state, bread_crumb_states = identify_pedagogical_state(ground_truth, MDP.prior_specification_fsm)
+    desired_state, bread_crumb_states = identify_pedagogical_state(ground_truth, MDP.specification_fsm)
 
     #Recompile the spec_fsm with the new reward`
-    spec_fsm2 = recompile_reward_function(MDP.specification_fsm, desired_state, breadcrumbs)
-    for state_id in breadcrumbs:
+    spec_fsm2 = recompile_reward_function(MDP.specification_fsm, desired_state, bread_crumb_states)
+    for state_id in bread_crumb_states:
         if spec_fsm2.id2states[state_id] in spec_fsm2.terminal_states:
             spec_fsm2.terminal_states.remove(spec_fsm2.id2states[state_id])
     if non_terminal:
@@ -72,17 +75,17 @@ def create_pedagogical_demo(ground_truth, MDP, n_threats = 0):
         #Return an independent sampled demonstrations
         specification_fsm = SpecificationFSM(formulas=[ground_truth], probs = [1])
         control_mdp = MDP.control_mdp
-        MDP = SpecificationMDP(specification_fsm, control_mdp)
+        MDP2 = SpecificationMDP(specification_fsm, control_mdp)
 
-        q_agent = QLearningAgent(MDP)
+        q_agent = QLearningAgent(MDP2)
         print('Training ground truth demonstrator')
         q_agent.explore(episode_limit = 5000, verbose=verbose, action_limit = 1000000)
-        eval_agent = ExplorerAgent(MDP, input_policy=q_agent.create_learned_softmax_policy(0.005))
+        eval_agent = ExplorerAgent(MDP2, input_policy=q_agent.create_learned_softmax_policy(0.005))
         print('\n')
-        eval_agent.explore(episode_limit = nDemo)
+        eval_agent.explore(episode_limit = 1)
 
         episode_record = eval_agent.episodic_record[0]
-        trace_slices = [MDP.control_mdp.create_observations(record[0][1]) for record in episode_record]
+        trace_slices = [MDP2.control_mdp.create_observations(record[0][1]) for record in episode_record]
         trace_slices.append(MDP.control_mdp.create_observations(episode_record[-1][2][1]))
 
         desired_state = None
