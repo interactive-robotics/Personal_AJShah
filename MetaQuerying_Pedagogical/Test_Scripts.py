fce8f85f8e53f5cf197d72912f759619ac99f935
ajshah91 <ajshah@mit.edu>
2021-03-08 12:33:10 -0500

    Update Test_Scripts.py

--- MetaQuerying_Pedagogical/Test_Scripts.py
+++ MetaQuerying_Pedagogical/Test_Scripts.py
@@ -8,23 +8,43 @@ Created on Tue Mar  2 15:47:37 2021
 from pedagogical_demo import *
 from query_selection import *
 from Auto_Eval_Active import *
+from tqdm import tqdm
 import os
 import dill
 
 directory =  'C:\\Users\\AJShah\\Documents\\GitHub\\Temporary'
 
-def import_data ():
+def find_query_mismatches(d, strat1, strat2):
     
-    with open(os.path.join(directory, 'example.pkl'), 'rb') as file:
-        data = dill.load(file)
-    return data
+    diverging_dists = []
+    
+    for i in tqdm(d['results'].keys()):
+        for c in set(d['results'][i].keys()) - set(['ground_truth']):
+            for q in range(len(d['results'][i][c])):
+                
+                dist = d['results'][i][c][q]
+                spec_fsm = SpecificationFSM(dist['formulas'], dist['probs'])
+                
+                s1 = identify_desired_state(spec_fsm, query_type = strat1)
+                s2 = identify_desired_state(spec_fsm, query_type = strat2)
+                
+                if s1 != s2:
+                    diverging_dists.append({'dist': dist})
+                    if s1 != spec_fsm.id2states[0] and s2 != spec_fsm.id2states[0]:
+                        diverging_dists[-1]['trivial'] = False
+                    
+    return diverging_dists
+                
 
 if __name__ == '__main__':
     
-    data = import_data()
-    dist = data['Distributions'][0]
+    print('Reading Results file \n')
+    with open(os.path.join(directory, 'active_paired_summary.pkl'),'rb') as file:
+        d = dill.load(file)
+        
     
-    spec_fsm = SpecificationFSM(dist['formulas'], dist['probs'])
-    ground_truth = data['ground_truth_formula']
+    print('Detecting Diverging queries')
+    dists = find_query_mismatches(d, 'uncertainty_sampling', 'info_gain')
     
-    d = compute_expected_entropy_gain_noisy_pedagogical(spec_fsm, debug = True)
+    
+    
