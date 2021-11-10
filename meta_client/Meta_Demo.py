3a72ca743bf685133e6270e7b1a4a1b92400154d
ajshah91 <ankitshah91@gmail.com>
2021-11-10 15:30:35 -0500

    minor changes

--- meta_client/Meta_Demo.py
+++ meta_client/Meta_Demo.py
@@ -1,9 +1,10 @@
 from puns_bsi_client_dinner import *
+from puns_bsi_client_dinner import Meta_demo
 import sys
 
 if __name__ == '__main__':
 
 	subject_id = sys.argv[1]
 	#active_trial_remote(nQuery=3, n_postdemo = 3, n_demo = 2)
-	meta_selection_trials(n_demo = 3, n_query = 3, n_postdemo = 2)
+	Meta_demo(n_demo = 3, n_query = 1, n_postdemo = 1)
 	record_subject_data(subject_id,'Lab_Demo')
