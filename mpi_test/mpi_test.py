6e34b7012b141e8821a631a6f0d471361cb38dac
ajshah91 <ankitshah91@gmail.com>
2022-03-08 14:24:02 -0500

    mpi tests added

---
+++ mpi_test/mpi_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 13:18:51 2022
+
+@author: ajshah
+"""
+from mpi4py import MPI
+import sys
+
+def print_hello(rank, size, name):
+  msg = "Hello World! I am process {0} of {1} on {2}.\n"
+  sys.stdout.write(msg.format(rank, size, name))
+
+if __name__ == "__main__":
+  size = MPI.COMM_WORLD.Get_size()
+  rank = MPI.COMM_WORLD.Get_rank()
+  name = MPI.Get_processor_name()
+
+  print_hello(rank, size, name)
