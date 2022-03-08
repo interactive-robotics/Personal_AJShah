6ef8284aefccc0d49fe464c11f89a1b48bb0d144
ajshah91 <ankitshah91@gmail.com>
2022-03-08 14:58:09 -0500

    timing

--- mpi_test/mpi_test.py
+++ mpi_test/mpi_test.py
@@ -6,18 +6,25 @@ Created on Tue Mar  8 13:18:51 2022
 @author: ajshah
 """
 from mpi4py import MPI
+from mpi4py.futures import MPIPoolExecutor
 import sys
+import os
+import time
 
 def print_hello(rank, size, name):
   msg = "Hello World! I am process {0} of {1} on {2}.\n"
   sys.stdout.write(msg.format(rank, size, name))
   
 def test_mpi_executor(n_cores = 48):
+    commands = [f'python run_single_test.py']
+    with MPIPoolExecutor(max_workers = n_cores) as executor:
+        retvals = executor.pool(os.system, commands)
+    retvals = list(retvals)
+    return retvals
     
 
 if __name__ == "__main__":
-  size = MPI.COMM_WORLD.Get_size()
-  rank = MPI.COMM_WORLD.Get_rank()
-  name = MPI.Get_processor_name()
-
-  print_hello(rank, size, name)
+    start = time.time()
+    retvals = test_mpi_executor(32)
+    end = time.time()
+    print('Time elapsed: ', end-start)
