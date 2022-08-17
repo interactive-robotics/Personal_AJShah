693c55d00a4f780a57a7066b38caccc615a14d90
Shen Li <shenlirobot@gmail.com>
2022-08-17 14:03:27 -0400

    add active demo, only doing active querying, for museum

--- meta_client/Meta_Demo.py
+++ meta_client/Meta_Demo.py
@@ -10,8 +10,11 @@ if __name__ == '__main__':
 
     subject_id = sys.argv[1]
     #active_trial_remote(nQuery=3, n_postdemo = 3, n_demo = 2)
-    Meta_demo(n_demo = 3, n_query = 1, n_postdemo = 1)
+    # Meta_demo(n_demo = 3, n_query = 1, n_postdemo = 1)
     #record_subject_data(subject_id,'Lab_Demo')
+
+    # 220817: Based on Shen and Ankit's email about <question about puns>:
+    Active_demo(n_demo = 3, n_query = 1, n_postdemo = 1)
 
     # In run_q_learning_agent_as_server_interactive.py, we send 1 to run_interactive_demo.py to start it. In Meta_Demo.py, we send 2 to run_interactive_demo.py to end it.
     sock_end = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
